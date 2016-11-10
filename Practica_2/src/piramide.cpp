88ca2de419be04e48452fef028941c00288d9e8a
Andres Molina <andresmag2@gmail.com>
2016-11-10 17:10:59 +0100

    Update
    Mejora para mejor utilización de la herencia

--- Practica_2/src/piramide.cpp
+++ Practica_2/src/piramide.cpp
@@ -10,13 +10,9 @@
 
 //Constructor
 Piramide::Piramide(){
-	perfil_piramide.resize(3);
-	perfil_piramide[0] = _vertex3f(0,0.5,0);
-	perfil_piramide[1] = _vertex3f(0.5,-0.5, 0);
-	perfil_piramide[2] = _vertex3f(0,-0.5,0);
+	figura_guardada.resize(3);
+	figura_guardada[0] = _vertex3f(0,0.5,0);
+	figura_guardada[1] = _vertex3f(0.5,-0.5, 0);
+	figura_guardada[2] = _vertex3f(0,-0.5,0);
 }
 
-//Funcion para obtener el perfil de la piramide
-vector<_vertex3f> Piramide::getPerfil(){
-	return perfil_piramide;
-}
